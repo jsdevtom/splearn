174cc4ebba9a42edc212668a2224d45483348626
1db8k <1db8k@live.com>
2017-04-23 12:55:18 +0200

    Modularize 'nextAssessmentDate' function

--- src/server/helpers.js
+++ src/server/helpers.js
@@ -23,8 +23,8 @@ module.exports.levenshtein = (a, b) => {
         val = row[j - 1] // match
       } else {
         val = Math.min(row[j - 1] + 1, // substitution
-              Math.min(prev + 1,     // insertion
-                       row[j] + 1))  // deletion
+          Math.min(prev + 1, // insertion
+            row[j] + 1)) // deletion
       }
       row[j - 1] = prev
       prev = val
@@ -32,4 +32,45 @@ module.exports.levenshtein = (a, b) => {
     row[a.length] = prev
   }
   return row[a.length]
+}
+
+module.exports.nextAssessmentDate = (numCorrectAttempts, lastAssessedDate) => {
+  const milliSecIntervals = [ 5000,
+    25000,
+    120000,
+    600000,
+    3600000,
+    18000000,
+    86400000,
+    432000000,
+    2160000000,
+    10512000000,
+    63072000000
+  ]
+  if (numCorrectAttempts < 0) {
+    return new Date(
+      lastAssessedDate.getTime() + milliSecIntervals[0]
+    )
+  } else if (numCorrectAttempts >= milliSecIntervals.length) {
+    return new Date(8640000000000000)
+  } else {
+    return new Date(
+      lastAssessedDate.getTime() + milliSecIntervals[numCorrectAttempts]
+    )
+  }
+}
+
+module.exports.attemptAnswer = (docObj, isCorrect) => {
+  let netCorrect = docObj.correctAttempts - docObj.wrongAttempts
+  let curTime = new Date()
+  return Promise.all([
+    docObj.update({ $inc: { [isCorrect ? 'correctAttempts' : 'wrongAttempts']: 1 } }, { upsert: true }),
+    docObj.update({ $set: { lastAssessed: curTime } }, { upsert: true })
+  ])
+  .then((results) => docObj.update({
+    $set: {
+      // toBeAssessedNext: module.exports.nextAssessmentDate(netCorrect, docObj.lastAssessed || docObj.createdAt)
+      toBeAssessedNext: module.exports.nextAssessmentDate(netCorrect, curTime)
+    }
+  }))
 }
