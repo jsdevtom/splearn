7dfe6f6b56b5e998be4762ec10903bfca65cd267
1db8k <1db8k@live.com>
2017-04-23 12:55:00 +0200

    Modularized routes

---
+++ src/server/api.routes.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const router = express.Router()
+const { levenshtein, attemptAnswer } = require('./helpers')
+// Models
+const QuestionAnswerPair = require('./models/questionAnswerPair.model')
+
+// select all
+router.get('/qapairs', function (req, res) {
+  QuestionAnswerPair.find()
+    .then((docs) => {
+      res.json(docs)
+    })
+    .catch((err) => console.error(err))
+})
+
+// find nth question
+router.get('/qapairs/question/:n', function (req, res) {
+  QuestionAnswerPair.findOne({}, 'question _id', { skip: +req.params.n })
+    .then((doc) => {
+      res.json(doc)
+    })
+    .catch((err) => console.error(err))
+})
+
+// currently obselete; logic in front end because the front end needs to get the all the answers on page load anyway.
+// get the answers to be reviewed
+router.get('/qapairs/due-to-be-reviewed', function (req, res) {
+  QuestionAnswerPair.find({
+    toBeAssessedNext: { $lte: new Date() }
+  })
+    .then((doc) => {
+      res.json(doc.length)
+    })
+    .catch((err) => console.error(err))
+})
+
+// create
+router.post('/qapairs', function (req, res) {
+  let obj = new QuestionAnswerPair(req.body)
+  obj.save()
+    .then((obj) => {
+      res.status(200).json(obj)
+    })
+    .catch((err) => console.error(err))
+})
+
+// check if answer is correct
+router.post('/qapairs/is_correct', function (req, res) {
+  let isCorrect
+
+  QuestionAnswerPair.findById(req.body.id)
+    .then((obj) => {
+      const correctAnswers = obj.correctAnswers
+      const wrongAnswers = obj.wrongAnswers
+      for (let i = 0; i < correctAnswers.length; i++) {
+        let correctAnswer = correctAnswers[i]
+
+        /*
+            If the submitted answer is the same as the correctAnswer but also not in the wrong answer array then send back isTrue. Example:
+              Question: Affect or Effect?
+              Answers:  How does the budget ______ people?
+
+              levenshtein('effect', 'affect') / 'effect'.length would return 0.167 here which would pass
+              thus !wrongAnswers.includes('affect') is needed to exclude 'affect' as a correct answer.
+        */
+        if ((levenshtein(correctAnswer, req.body.answer) / correctAnswer.length) <= 0.25 &&
+          !wrongAnswers.includes(req.body.answer)) {
+          isCorrect = true
+          return attemptAnswer(obj, isCorrect)
+        }
+      }
+      isCorrect = false
+      return attemptAnswer(obj, isCorrect)
+    })
+    .then((updateResult) => {
+      return res.status(200).json({ isCorrect })
+    })
+    .catch((err) => console.error(err))
+})
+
+// update by id
+router.put('/qapairs/:id', function (req, res) {
+  QuestionAnswerPair.findById(req.params.id)
+    .then((obj) => {
+      obj.question = req.body.question
+      obj.correctAnswers = req.body.correctAnswers
+      obj.wrongAnswers = req.body.wrongAnswers
+      return obj.save()
+    })
+    .then((obj) => res.status(200).json(obj))
+    .catch((err) => console.error(err))
+})
+
+// delete by id
+router.delete('/qapairs/:id', function (req, res) {
+  QuestionAnswerPair.findByIdAndRemove(req.params.id)
+    .then((deletedObj) => {
+      res.status(200).json(deletedObj)
+    })
+    .catch((err) => console.error(err))
+})
+
+module.exports = router
--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan') // logger
 const bodyParser = require('body-parser')
-const { levenshtein } = require('./helpers')
+const apiRoutes = require('./api.routes')
 
 const app = express()
 app.set('port', (process.env.PORT || 3000))
@@ -14,94 +14,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(morgan('dev'))
 
-const mongoose = require('mongoose')
+const { mongoose } = require('./imports')
 mongoose.connect('mongodb://localhost:27017/quiz')
 const db = mongoose.connection
-mongoose.Promise = global.Promise
-
-// Models
-const QuestionAnswerPair = require('./models/questionAnswerPair.model')
 
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {
   console.log('Connected to MongoDB')
 
   // // APIs
-  // // select all
-  app.get('/api/qapairs', function (req, res) {
-    QuestionAnswerPair.find()
-      .then((docs) => {
-        res.json(docs)
-      })
-      .catch((err) => console.error(err))
-  })
-
-  // find nth question
-  app.get('/api/qapairs/question/:n', function (req, res) {
-    QuestionAnswerPair.findOne({}, 'question _id', { skip: +req.params.n })
-      .then((doc) => {
-        res.json(doc)
-      })
-      .catch((err) => console.error(err))
-  })
-
-  // create
-  app.post('/api/qapairs', function (req, res) {
-    let obj = new QuestionAnswerPair(req.body)
-    obj.save()
-      .then((obj) => {
-        res.status(200).json(obj)
-      })
-      .catch((err) => console.error(err))
-  })
-
-  // check if answer is correct
-  app.post('/api/qapairs/isCorrect', function (req, res) {
-    QuestionAnswerPair.findById(req.body.id)
-      .then((obj) => {
-        const correctAnswers = obj.correctAnswers
-        const wrongAnswers = obj.wrongAnswers
-        for (let i = 0; i < correctAnswers.length; i++) {
-          let correctAnswer = correctAnswers[i]
-
-          //  If the submitted answer is the same as the correctAnswer but also not in the wrong answer array
-          //  then send back isTrue. Example:
-          //    Question: Affect or Effect?
-          //    Answers:  How does the budget ______ people?
-          //
-          //    levenshtein('effect', 'affect') / 'effect'.length would return 0.167 here which would pass
-          //    thus !wrongAnswers.includes('affect') is needed to overcome this.
-          if ((levenshtein(correctAnswer, req.body.answer) / correctAnswer.length) <= 0.25 &&
-          !wrongAnswers.includes(req.body.answer)) {
-            return res.status(200).json({isTrue: true})
-          }
-        }
-        return res.status(200).json({isTrue: false})
-      })
-      .catch((err) => console.error(err))
-  })
-
-  // update by id
-  app.put('/api/qapairs/:id', function (req, res) {
-    QuestionAnswerPair.findById(req.params.id)
-      .then((obj) => {
-        obj.question = req.body.question
-        obj.correctAnswers = req.body.correctAnswers
-        obj.wrongAnswers = req.body.wrongAnswers
-        return obj.save()
-      })
-      .then((obj) => res.status(200).json(obj))
-      .catch((err) => console.error(err))
-  })
-
-  // delete by id
-  app.delete('/api/qapairs/:id', function (req, res) {
-    QuestionAnswerPair.findByIdAndRemove(req.params.id)
-      .then((deletedObj) => {
-        res.status(200).json(deletedObj)
-      })
-      .catch((err) => console.error(err))
-  })
+  app.use('/api', apiRoutes)
 
   // all other routes are handled by Angular
   app.get('/*', function (req, res) {
